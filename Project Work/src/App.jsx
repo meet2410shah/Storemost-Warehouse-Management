2554d9a41d1ff88b86b509217844104920097294
Meet Shah <meet2410shah@gmail.com>
2021-04-02 23:53:13 +0530

    defalut env added

--- Project Work/src/App.jsx
+++ Project Work/src/App.jsx
@@ -16,7 +16,7 @@ class App extends React.Component {
         return(
          <BrowserRouter>
             <Switch>
-                <Route exact path="/" component={Details}/>
+                <Route exact path="/" component={Login}/>
                 <Route path="/login" component={Login}/>
                 <Route path="/register" component={Register}/>
                 <Route path="/details" component={Details}/>
