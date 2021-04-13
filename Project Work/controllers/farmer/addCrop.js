917c90ed9da8ae0a91d3ccd4b212962bf3e99635
Meet Shah <meet2410shah@gmail.com>
2021-04-13 16:43:42 +0530

    Farmer - Add Crop Warehouse Checking Added

--- Project Work/controllers/farmer/addCrop.js
+++ Project Work/controllers/farmer/addCrop.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const { farmerUser } = require('../../database/models/farmer');
+const { warehouseUser } = require('../../database/models/warehouse');
 
 module.exports = async (req, res) => {
 	const token = req.cookies.token;
@@ -16,45 +17,9 @@ module.exports = async (req, res) => {
 	}
 
 	// Check if Token is not corrupted
+	let data = null;
 	try {
-		const data = jwt.verify(token, process.env.SECRET);
-		const userId = data.userId;
-
-		const crop = {
-			storageTime: Date.now(),
-			quantity: req.body.quantity,
-			warehouseId: req.body.warehouseId,
-			paymentId: req.body.paymentId,
-			cropType: req.body.cropType,
-		};
-
-		await farmerUser.findOneAndUpdate(
-			{ _id: userId },
-			{
-				$push: {
-					crops: crop,
-				},
-			},
-			(err, data) => {
-				if (err) {
-					return res.send({
-						success: false,
-						data: null,
-						err: {
-							code: 1007,
-							msg: 'Not able to add the crop! Sorry of Inconvinence',
-						},
-					});
-				}
-				return res.send({
-					success: true,
-					data: {
-						msg: 'Crop Added Successfully',
-					},
-					err: null,
-				});
-			}
-		);
+		data = jwt.verify(token, process.env.SECRET);
 	} catch (err) {
 		return res.send({
 			success: false,
@@ -65,4 +30,67 @@ module.exports = async (req, res) => {
 			},
 		});
 	}
+
+	// Check if the warehouse is there or not.
+	const userId = data.userId;
+	try {
+		data = await warehouseUser.find({
+			warehouseId: req.body.warehouseId,
+		});
+	} catch (err) {
+		return res.send({
+			success: false,
+			data: null,
+			err: {
+				code: 1007,
+				msg: 'Database Error',
+			},
+		});
+	}
+	if (data.length === 0) {
+		return res.send({
+			success: false,
+			data: null,
+			err: {
+				code: 1008,
+				msg: "Warehouse does'nt exist",
+			},
+		});
+	}
+
+	const crop = {
+		storageTime: Date.now(),
+		quantity: req.body.quantity,
+		warehouseId: req.body.warehouseId,
+		paymentId: req.body.paymentId,
+		cropType: req.body.cropType,
+	};
+
+	try {
+		data = await farmerUser.findOneAndUpdate(
+			{ _id: userId },
+			{
+				$push: {
+					crops: crop,
+				},
+			}
+		);
+	} catch (err) {
+		return res.send({
+			success: false,
+			data: null,
+			err: {
+				code: 1007,
+				msg: 'Not able to add the crop! Sorry of Inconvinence',
+			},
+		});
+	}
+
+	return res.send({
+		success: true,
+		data: {
+			msg: 'Crop Added Successfully',
+		},
+		err: null,
+	});
 };
