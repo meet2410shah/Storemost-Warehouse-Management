2db8637b4838f9dec19d6cc4be4f03f746d7f066
MahakshTrivedi <mahakshtrivedi@Mahakshs-MacBook-Pro.local>
2021-04-15 14:25:03 +0530

    Updated Implementation with latest changes

---
+++ Project Work/client/src/components/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import ProfilePage from "./components/ProfilePage";
+// import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <ProfilePage />
+    </div>
+  );
+}
+
+export default App;
