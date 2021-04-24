5ea87bc8adf8e58840d0caecbfb197eb719586fa
Bhavyadnc <bhavyavinj@gmail.com>
2021-04-24 22:47:30 +0530

    Frontend pages in ejs

--- Project Work/index.js
+++ Project Work/index.js
@@ -9,6 +9,7 @@ const app = express();
 const version = process.env.VERSION;
 const PORT = process.env.PORT || 5000;
 
+app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(cors());
 
@@ -19,12 +20,34 @@ const farmer = require('./routes/farmer');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('dist'));
+app.use(express.static('public'));
 
 // Routes
 app.use(`${version}/admin`, admin);
 app.use(`${version}/supervisor`, supervisor);
 app.use(`${version}/farmer`, farmer);
+
+const warehouseList = require("./testData/WarehouseList")
+
+app.get('/view', (req, res) => {
+	res.render("ViewProfile");
+});
+
+app.get('/edit', (req, res) => {
+	res.render("EditProfile");
+});
+
+app.get('/ware', (req, res) => {
+	res.render("warehouseList",{warehouseList: warehouseList});
+});
+
+app.get('/login', (req, res) => {
+	res.render("Login");
+});
+
+app.get('/', (req, res) => {
+	res.render("AdminSidePage");
+});
 
 app.listen(PORT, () => {
 	console.log('Server Listening on PORT ' + PORT);
---
+++ Project Work/testData/WarehouseList.js
@@ -0,0 +1,60 @@
+const warehouseList = [
+  {
+    warehouseId: 1,
+    capacity: "3000 Quintal",
+    available: "1020 Quintal",
+    address: "Lorem ip. Ut enim ad minnt in culpa qui officia deserunt mollit anim id est laborum.",
+    mobile: "+2736487903870"
+  },
+  {
+    warehouseId: 2,
+    capacity: "6020 Quintal",
+    available: "1000 Quintal",
+    address: "Lorem ip. Ut enim ad minnt in culpa qui officia deserunt mollit anim qui officia deserunt mollit anim id est laborum.",
+    mobile: "+54780903870"
+  },
+  {
+    warehouseId: 3,
+    capacity: "1000 Quintal",
+    available: "100 Quintal",
+    address: "Lorem ip. Ut enim ad minnt in culpa qui officia deserunt mollit anim id est laborum.",
+    mobile: "+54677903870"
+  },
+  {
+    warehouseId: 4,
+    capacity: "10000 Quintal",
+    available: "2056 Quintal",
+    address: "Lorem ip. Ut enim ad minnt in culpa qui officia deserunt mollit animqui officia deserunt mollit animqui officia deserunt mollit anim id est laborum.",
+    mobile: "+09809487903870"
+  },
+  {
+    warehouseId: 5,
+    capacity: "10000 Quintal",
+    available: "2056 Quintal",
+    address: "Lorem ip. Ut enim ad minnt in culpa qui officia deserunt mollit animqui officia deserunt mollit animqui officia deserunt mollit anim id est laborum.",
+    mobile: "+09809487903870"
+  },
+  {
+    warehouseId: 6,
+    capacity: "10000 Quintal",
+    available: "2056 Quintal",
+    address: "Lorem ip. Ut enim ad minnt in culpa qui officia deserunt mollit animqui officia deserunt mollit animqui officia deserunt mollit anim id est laborum.",
+    mobile: "+09809487903870"
+  },
+  {
+    warehouseId: 7,
+    capacity: "10000 Quintal",
+    available: "2056 Quintal",
+    address: "Lorem ip. Ut enim ad minnt in culpa qui officia deserunt mollit animqui officia deserunt mollit animqui officia deserunt mollit anim id est laborum.",
+    mobile: "+09809487903870"
+  },
+  {
+    warehouseId: 8,
+    capacity: "10000 Quintal",
+    available: "2056 Quintal",
+    address: "Lorem ip. Ut enim ad minnt in culpa qui officia deserunt mollit animqui officia deserunt mollit animqui officia deserunt mollit anim id est laborum.",
+    mobile: "+09809487903870"
+  },
+];
+
+module.exports = warehouseList;
