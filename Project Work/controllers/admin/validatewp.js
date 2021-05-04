43096f688e5b63c80067c44b4eca705314099e8b
Meet Shah <meet2410shah@gmail.com>
2021-05-04 10:22:52 +0530

    Package.json added for Heroku

--- Project Work/controllers/admin/validatewp.js
+++ Project Work/controllers/admin/validatewp.js
@@ -3,18 +3,20 @@ const Joi = require('joi');
 // This function checks the validity of the inputs of admin during registration
 
 function validateUser(user) {
-    const schema = Joi.object({
-        firstName: Joi.string().min(3).max(50).required(),
-        lastName: Joi.string().min(3).max(50).required(),
-        username: Joi.string().min(5).max(50).required()
-            .token(),
-        email: Joi.string().min(5).max(255).required()
-            .email(),
-        mobile: Joi.string().min(5).max(50).required()
-            .pattern(/^[0-9]+$/),
-    });
+	const schema = Joi.object({
+		firstName: Joi.string().min(3).max(50).required(),
+		lastName: Joi.string().min(3).max(50).required(),
+		username: Joi.string().min(5).max(50).required().token(),
+		email: Joi.string().min(5).max(255).required().email(),
+		mobile: Joi.string()
+			.min(5)
+			.max(50)
+			.required()
+			.pattern(/^[0-9]+$/),
+		address: Joi.string().max(400),
+	});
 
-    return schema.validate(user);
+	return schema.validate(user);
 }
 
 exports.validatewp = validateUser;
